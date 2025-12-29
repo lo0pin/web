c62d94c31f8c6e55ab7d2832822be105f4bea882
Julian Kampitsch <julian.kampitsch@ms.bulme.at>
2025-12-29 21:55:24 +0100

    files created

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Main.js geladen ✔️");
+
+  // Platz für spätere Initialisierung:
+  // initNavigation();
+  // initThemeToggle();
+});
